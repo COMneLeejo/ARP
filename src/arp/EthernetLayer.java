4c2413b024a2c2751e87165f0e667d0683dfe17a
woocheol Kwon <ddft97@gmail.com>
2021-10-07 03:56:56 +0900

    [refactor]Ethernet&NI Layer 컨벤션 적용

---
+++ src/arp/EthernetLayer.java
@@ -0,0 +1,186 @@
+package arp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class EthernetLayer implements BaseLayer {
+    public int n_upper_layer_count = 0;
+    public String p_layer_name = null;
+    public BaseLayer p_under_layer = null;
+    public ArrayList<BaseLayer> p_upper_layer = new ArrayList<BaseLayer>();
+    public final static int HEARER_SIZE = 14;
+    private static byte[] arp_mac_dstaddr = null;
+
+    public byte[] chat_file_dstaddr;
+    public byte[] ex_ethernetaddr = new byte[6];
+
+    private class _ETHERNET_ADDR {
+        private byte[] addr = new byte[6];
+
+        public _ETHERNET_ADDR() {
+            this.addr[0] = (byte) 0x00;
+            this.addr[1] = (byte) 0x00;
+            this.addr[2] = (byte) 0x00;
+            this.addr[3] = (byte) 0x00;
+            this.addr[4] = (byte) 0x00;
+            this.addr[5] = (byte) 0x00;
+        }
+    }
+
+    private class _ETHERNET_HEADER {
+        _ETHERNET_ADDR enet_dstaddr;
+        _ETHERNET_ADDR enet_srcaddr;
+        byte[] enet_type;
+        byte[] enet_data;
+
+        public _ETHERNET_HEADER() {
+            this.enet_dstaddr = new _ETHERNET_ADDR();
+            this.enet_srcaddr = new _ETHERNET_ADDR();
+            this.enet_type = new byte[2];
+            this.enet_data = null;
+        }
+    }
+
+    _ETHERNET_HEADER m_sHeader = new _ETHERNET_HEADER();
+
+    public EthernetLayer(String pName) {
+        // super(pName);
+        // TODO Auto-generated constructor stub
+        p_layer_name = pName;
+
+    }
+
+    public void setEnetSrcAddress(byte[] srcAddress) {
+        // TODO Auto-generated method stub
+        m_sHeader.enet_srcaddr.addr[0] = srcAddress[0];
+        m_sHeader.enet_srcaddr.addr[1] = srcAddress[1];
+        m_sHeader.enet_srcaddr.addr[2] = srcAddress[2];
+        m_sHeader.enet_srcaddr.addr[3] = srcAddress[3];
+        m_sHeader.enet_srcaddr.addr[4] = srcAddress[4];
+        m_sHeader.enet_srcaddr.addr[5] = srcAddress[5];
+
+    }
+
+    public void setEnetDstAddress(byte[] dstAddress) {
+        // TODO Auto-generated method stub
+        m_sHeader.enet_dstaddr.addr[0] = dstAddress[0];
+        m_sHeader.enet_dstaddr.addr[1] = dstAddress[1];
+        m_sHeader.enet_dstaddr.addr[2] = dstAddress[2];
+        m_sHeader.enet_dstaddr.addr[3] = dstAddress[3];
+        m_sHeader.enet_dstaddr.addr[4] = dstAddress[4];
+        m_sHeader.enet_dstaddr.addr[5] = dstAddress[5];
+
+    }
+
+    public static void setMacAddrDstAddr(byte[] dstAddress) {
+        arp_mac_dstaddr = dstAddress;
+    }
+
+    public byte[] ObjToByteDATA(_ETHERNET_HEADER Header, byte[] input, int length) {
+        byte[] buf = new byte[length + HEARER_SIZE];
+
+        buf[0] = Header.enet_dstaddr.addr[0];
+        buf[1] = Header.enet_dstaddr.addr[1];
+        buf[2] = Header.enet_dstaddr.addr[2];
+        buf[3] = Header.enet_dstaddr.addr[3];
+        buf[4] = Header.enet_dstaddr.addr[4];
+        buf[5] = Header.enet_dstaddr.addr[5];
+        buf[6] = Header.enet_srcaddr.addr[0];
+        buf[7] = Header.enet_srcaddr.addr[1];
+        buf[8] = Header.enet_srcaddr.addr[2];
+        buf[9] = Header.enet_srcaddr.addr[3];
+        buf[10] = Header.enet_srcaddr.addr[4];
+        buf[11] = Header.enet_srcaddr.addr[5];
+        buf[12] = Header.enet_type[0];
+        buf[13] = Header.enet_type[1];
+
+        for (int i = 0; i < length; i++) {
+            buf[HEARER_SIZE + i] = input[i];
+        }
+        return buf;
+    }
+
+    public boolean send(byte[] input, int length) {
+
+
+        return true;
+    }
+
+
+    public byte[] removeCappHeader(byte[] input, int length) {
+
+        byte[] rebuf = new byte[length - HEARER_SIZE];
+
+        return rebuf;
+    }
+
+    public boolean receive(byte[] input) {
+
+        return true;
+    }
+
+    public boolean dstme_Addr(byte[] add) {
+
+        return true;
+    }
+
+    public boolean srcme_Addr(byte[] add) {
+
+        return true;
+    }
+
+    public boolean dst_you(byte[] add) {// 二쇱냼�솗�씤
+
+        return true;
+    }
+
+    public boolean bro_Addr(byte[] add) {// 二쇱냼�솗�씤
+
+        return true;
+    }
+
+    @Override
+    public void setUnderLayer(BaseLayer pUnderLayer) {
+        if (pUnderLayer == null)
+            return;
+        p_under_layer = pUnderLayer;
+    }
+
+    @Override
+    public void setUpperLayer(BaseLayer pUpperLayer) {
+        if (pUpperLayer == null)
+            return;
+        this.p_upper_layer.add(n_upper_layer_count++, pUpperLayer);
+    }
+
+    @Override
+    public String getLayerName() {
+        // TODO Auto-generated method stub
+        return p_layer_name;
+    }
+
+    @Override
+    public BaseLayer getUnderLayer() {
+        if (p_under_layer == null)
+            return null;
+        return p_under_layer;
+    }
+
+    @Override
+    public BaseLayer getUpperLayer(int nindex) {
+        if (nindex < 0 || nindex > n_upper_layer_count || n_upper_layer_count < 0)
+            return null;
+        return p_upper_layer.get(nindex);
+    }
+
+    @Override
+    public void setUpperUnderLayer(BaseLayer pUULayer) {
+        this.SetUpperLayer(pUULayer);
+        pUULayer.SetUnderLayer(this);
+    }
+
+    @Override
+    public BaseLayer getUnderLayer(int nindex) {
+        return null;
+    }
+}
---
+++ src/arp/NILayer.java
@@ -0,0 +1,125 @@
+package arp;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapIf;
+import org.jnetpcap.packet.PcapPacket;
+import org.jnetpcap.packet.PcapPacketHandler;
+
+public class NILayer implements BaseLayer {
+
+    public int nUpperLayerCount = 0;
+    public String pLayerName = null;
+    public BaseLayer p_UnderLayer = null;
+    public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>();
+    private Receive_Thread thread = null;
+    int m_iNumAdapter;
+    public Pcap m_AdapterObject;
+    public PcapIf device;
+    public List<PcapIf> m_pAdapterList;
+    StringBuilder errbuf = new StringBuilder();
+
+
+    public void packetStartDriver() {
+
+    }
+
+    public PcapIf getAdapterObject(int iIndex) {
+        return m_pAdapterList.get(iIndex);
+    }
+
+    public void setAdapterNumber(int iNum) {
+
+    }
+
+    public void setAdapterList() {
+
+
+    }
+
+    public boolean send(byte[] input, int length) {
+
+        return true;
+    }
+
+    public boolean receive() {
+        return true;
+    }
+
+    @Override
+    public void setUnderLayer(BaseLayer pUnderLayer) {
+        // TODO Auto-generated method stub
+        if (pUnderLayer == null)
+            return;
+        p_UnderLayer = pUnderLayer;
+    }
+
+    @Override
+    public void setUpperLayer(BaseLayer pUpperLayer) {
+        // TODO Auto-generated method stub
+        if (pUpperLayer == null)
+            return;
+        this.p_aUpperLayer.add(nUpperLayerCount++, pUpperLayer);
+    }
+
+    @Override
+    public String getLayerName() {
+        // TODO Auto-generated method stub
+        return pLayerName;
+    }
+
+    @Override
+    public BaseLayer getUnderLayer() {
+        if (p_UnderLayer == null)
+            return null;
+        return p_UnderLayer;
+    }
+
+    @Override
+    public BaseLayer getUpperLayer(int nindex) {
+        // TODO Auto-generated method stub
+        if (nindex < 0 || nindex > nUpperLayerCount || nUpperLayerCount < 0)
+            return null;
+        return p_aUpperLayer.get(nindex);
+    }
+
+    @Override
+    public void setUpperUnderLayer(BaseLayer pUULayer) {
+
+    }
+
+    @Override
+    public BaseLayer getUnderLayer(int nindex) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
+
+class Receive_Thread implements Runnable {
+    byte[] data;
+    Pcap AdapterObject;
+    BaseLayer UpperLayer;
+
+    public Receive_Thread(Pcap m_AdapterObject, BaseLayer m_UpperLayer) {
+        // TODO Auto-generated constructor stub
+        AdapterObject = m_AdapterObject;
+        UpperLayer = m_UpperLayer;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() {
+                public void nextPacket(PcapPacket packet, String user) {
+                    data = packet.getByteArray(0, packet.size());
+                    UpperLayer.receive(data);
+                }
+            };
+
+            AdapterObject.loop(100000, jpacketHandler, "");
+        }
+    }
+}
