59c5859232dd9e2c102095c21a8b38cdb0457e1a
MoonDD99 <mjh991016@naver.com>
2021-10-10 15:25:23 +0900

    [feat] 최초구동 확인

--- src/arp/ApplicationLayer.java
+++ src/arp/ApplicationLayer.java
@@ -1,4 +1,4 @@
-package arp;
+
 
 import java.awt.Color;
 import java.awt.Container;
@@ -109,10 +109,10 @@ public class ApplicationLayer extends JFrame implements BaseLayer {
 
   		m_layer_mgr.connectLayers(" NI ( *Ethernet ( *ARP ( *IP ( +APP ( -IP ) ) ) *IP ( +APP ( -IP ) ) ) ) ");
 
-  		System.out.println(((IPLayer) m_layer_mgr.getLayer("IP")).GetUnderLayer(0).getLayerName()); // ARP
-  		System.out.println(((IPLayer) m_layer_mgr.getLayer("IP")).GetUnderLayer(1).getLayerName()); // Ethernet
-  		System.out.println(((EthernetLayer)m_layer_mgr.getLayer("Ethernet")).GetUpperLayer(0).getLayerName()); // ARP
-  		System.out.println(((EthernetLayer)m_layer_mgr.getLayer("Ethernet")).GetUpperLayer(1).getLayerName()); // IP
+  		System.out.println(((IPLayer) m_layer_mgr.getLayer("IP")).getUnderLayer(0).getLayerName()); // ARP
+  		System.out.println(((IPLayer) m_layer_mgr.getLayer("IP")).getUnderLayer(1).getLayerName()); // Ethernet
+  		System.out.println(((EthernetLayer)m_layer_mgr.getLayer("Ethernet")).getUpperLayer(0).getLayerName()); // ARP
+  		System.out.println(((EthernetLayer)m_layer_mgr.getLayer("Ethernet")).getUpperLayer(1).getLayerName()); // IP
   	}
 
     public ApplicationLayer (String pName) {
@@ -151,12 +151,12 @@ public class ApplicationLayer extends JFrame implements BaseLayer {
 	    	public void actionPerformed(ActionEvent arg0) {
 	            String delete_ip = JOptionPane.showInputDialog("Item's IP Address");
 	            if(delete_ip != null) {
-	            	if(((ARPLayer) m_layer_mgr.getLayer("ARP")).cacheTable.containsKey(delete_ip)) {
-	            		Object[] value = ((ARPLayer) m_layer_mgr.getLayer("ARP")).cacheTable.get(delete_ip);
+	            	if(((ARPLayer) m_layer_mgr.getLayer("ARP")).cache_table.containsKey(delete_ip)) {
+	            		Object[] value = ((ARPLayer) m_layer_mgr.getLayer("ARP")).cache_table.get(delete_ip);
 	            		if(System.currentTimeMillis()-(long)value[3]/1000 > 1) {
 	            			// cache table에서 입력한 ip주소에 해당하는 값 제거
-	            			((ARPLayer) m_layer_mgr.getLayer("ARP")).cacheTable.remove(delete_ip);
-	            			((ARPLayer) m_layer_mgr.getLayer("ARP")).updateARPCacheTable();
+	            			((ARPLayer) m_layer_mgr.getLayer("ARP")).cache_table.remove(delete_ip);
+	            			((ARPLayer) m_layer_mgr.getLayer("ARP")).updateCacheTable();
 	            		}
 	            	}
 	            }
@@ -169,11 +169,11 @@ public class ApplicationLayer extends JFrame implements BaseLayer {
 	    arp_cache_title.add(all_item_delete_button);
 	    all_item_delete_button.addActionListener(new ActionListener() {
 	    	public void actionPerformed(ActionEvent arg0) {
-	    		Set key = ((ARPLayer) m_layer_mgr.getLayer("ARP")).cacheTable.keySet();
+	    		Set key = ((ARPLayer) m_layer_mgr.getLayer("ARP")).cache_table.keySet();
 	            ArrayList<String> delete_key = new ArrayList<String>();
 	            for(Iterator iterator = key.iterator();iterator.hasNext();) {
 	            	String key_value = (String)iterator.next();
-	            	Object[] value = ((ARPLayer) m_layer_mgr.getLayer("ARP")).cacheTable.get(key_value);
+	            	Object[] value = ((ARPLayer) m_layer_mgr.getLayer("ARP")).cache_table.get(key_value);
 	            	if(System.currentTimeMillis()-(long)value[3]/100 <= 5) {
 	            		try {
 	            			Thread.sleep(System.currentTimeMillis()-(long)value[3]);
@@ -185,8 +185,8 @@ public class ApplicationLayer extends JFrame implements BaseLayer {
 	            }
 	            
 	            // cacheTable에서 deleteKey에 담긴 키 값들을 가진 bucket 삭제
-	            for(int i=0;i<delete_key.size();i++) ((ARPLayer) m_layer_mgr.getLayer("ARP")).cacheTable.remove(delete_key.get(i));
-	            ((ARPLayer) m_layer_mgr.getLayer("ARP")).updateARPCacheTable();
+	            for(int i=0;i<delete_key.size();i++) ((ARPLayer) m_layer_mgr.getLayer("ARP")).cache_table.remove(delete_key.get(i));
+	            ((ARPLayer) m_layer_mgr.getLayer("ARP")).updateCacheTable();
 	    	}
 	    });
 	    
@@ -216,7 +216,7 @@ public class ApplicationLayer extends JFrame implements BaseLayer {
 					byte[] ipAddr_dst = new byte[4];
 					for(int i=0;i<4;i++) ipAddr_dst[i] = (byte)Integer.parseInt(ipAddr_st[i]);
 
-					((IPLayer) m_layer_mgr.getLayer("IP")).SetIPDstAddress(ipAddr_dst);
+					((IPLayer) m_layer_mgr.getLayer("IP")).setIPDstAddress(ipAddr_dst);
 					
 					// IP계층의 Send함수 호출
 					under_layer.send(bytes, bytes.length);
@@ -285,7 +285,7 @@ public class ApplicationLayer extends JFrame implements BaseLayer {
 			public void actionPerformed(ActionEvent e) {
 				ARPLayer arpLayer = (ARPLayer) m_layer_mgr.getLayer("ARP");
 				// ARP계층의 proxyTable을 가져온다.
-				HashMap<String, Object[] > proxyTable = ((ARPLayer) m_layer_mgr.getLayer("ARP")).proxyTable;
+				HashMap<String, Object[] > proxyTable = ((ARPLayer) m_layer_mgr.getLayer("ARP")).proxy_table;
 				
 				// 빈 값이 없는지 조건검사
 				// ip가 4자리, mac주소가 6자리가 아니라면 에러가 발생한다
@@ -348,18 +348,18 @@ public class ApplicationLayer extends JFrame implements BaseLayer {
 			public void actionPerformed(ActionEvent arg0) {
 				String delete_ip = JOptionPane.showInputDialog("Host's IP Address");
 				if(delete_ip != null) {
-					if(((ARPLayer) m_layer_mgr.getLayer("ARP")).proxyTable.containsKey(delete_ip)) {
+					if(((ARPLayer) m_layer_mgr.getLayer("ARP")).proxy_table.containsKey(delete_ip)) {
 						// 테이블에서 제거
-						((ARPLayer) m_layer_mgr.getLayer("ARP")).proxyTable.remove(delete_ip);
+						((ARPLayer) m_layer_mgr.getLayer("ARP")).proxy_table.remove(delete_ip);
 						
 						// 삭제 후 Proxy table 출력창 업데이트
 						// cache table은 ARP계층에 updateARPCacheTable함수가 있었지만 proxy table업데이트 함수는 없음
 						String print_result ="";
-						for(Iterator iterator = ((ARPLayer) m_layer_mgr.getLayer("ARP")).proxyTable.keySet().iterator(); iterator.hasNext();) {
+						for(Iterator iterator = ((ARPLayer) m_layer_mgr.getLayer("ARP")).proxy_table.keySet().iterator(); iterator.hasNext();) {
 							String key_ip = (String)iterator.next();
-							Object[] obj = ((ARPLayer) m_layer_mgr.getLayer("ARP")).proxyTable.get(key_ip);
+							Object[] obj = ((ARPLayer) m_layer_mgr.getLayer("ARP")).proxy_table.get(key_ip);
 							print_result = print_result+"    "+(String)obj[0]+"\t";
-							byte[] mac = (byte[])((ARPLayer) m_layer_mgr.getLayer("ARP")).proxyTable.get(key_ip)[1];
+							byte[] mac = (byte[])((ARPLayer) m_layer_mgr.getLayer("ARP")).proxy_table.get(key_ip)[1];
 							String ip_String =key_ip;
 							String mac_String ="";
 							
@@ -368,7 +368,7 @@ public class ApplicationLayer extends JFrame implements BaseLayer {
 							
 							print_result = print_result+ip_String+"\t    "+mac_String+"\n";
 						}
-						int proxy_size = ((ARPLayer) m_layer_mgr.getLayer("ARP")).proxyTable.size();
+						int proxy_size = ((ARPLayer) m_layer_mgr.getLayer("ARP")).proxy_table.size();
 						proxy_area.setText(print_result);
 					}
 				}
@@ -453,12 +453,13 @@ public class ApplicationLayer extends JFrame implements BaseLayer {
 	        	           		ip_src[i] = (byte) Integer.parseInt(values_ip_source[i]);
 	        	           	}
 
-	        	           	((EthernetLayer) m_layer_mgr.getLayer("Ethernet")).SetEnetSrcAddress(ethernet_src);
-	        	           	((ARPLayer) m_layer_mgr.getLayer("ARP")).SetMacAddrSrcAddr(ethernet_src);
+	        	           	((EthernetLayer) m_layer_mgr.getLayer("Ethernet")).setEnetSrcAddress(ethernet_src);
+	        	           	((ARPLayer) m_layer_mgr.getLayer("ARP")).setHostMacAddr(ethernet_src);	// 자기 자신의 mac 주소 설정
 
-	        	           	((IPLayer) m_layer_mgr.getLayer("IP")).SetIPSrcAddress(ip_src);
-	        	           	((ARPLayer) m_layer_mgr.getLayer("ARP")).SetIPAddrSrcAddr(ip_src);
+	        	           	((IPLayer) m_layer_mgr.getLayer("IP")).setIPSrcAddress(ip_src);			// 자기 자신의 ip 주소 설정
+	        	           	((ARPLayer) m_layer_mgr.getLayer("ARP")).setHostIpAddr(ip_src);
 //	        	           	((NILayer) m_layer_mgr.getLayer("NI")).SetAdapterNumber(adapter_number);
+
 	        		 }
 	            } else {
 	            	JOptionPane.showMessageDialog(null, "H_W 주소를 입력해주십시오");
@@ -489,68 +490,68 @@ public class ApplicationLayer extends JFrame implements BaseLayer {
 	    nic_combo_box = new JComboBox();
 		nic_combo_box.setBounds(150, 110, 165, 20);
 		address_panel.add(nic_combo_box);
-//
-//		for (int i = 0; ((NILayer) m_layer_mgr.getLayer("NI")).getAdapterList().size() > i; i++) {
-//			PcapIf pcapIf = ((NILayer) m_layer_mgr.getLayer("NI")).GetAdapterObject(i);
-//			nic_combo_box.addItem(pcapIf.getName());
-//		}
-//
-//		nic_combo_box.addActionListener(new ActionListener() { // Event Listener
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				JComboBox jcombo = (JComboBox) e.getSource();
-//				adapter_number = jcombo.getSelectedIndex();
-//				System.out.println("Index: " + adapter_number);
-//				try {
-//					ethernet_src_address.setText("");
-//					ethernet_src_address.append(get_MacAddress(((NILayer) m_layer_mgr.getLayer("NI"))
-//							.GetAdapterObject(adapter_number).getHardwareAddress()));
-//
-//				} catch (IOException e1) {
-//					e1.printStackTrace();
-//				}
-//			}
-//		});
+
+		for (int i = 0; ((NILayer) m_layer_mgr.getLayer("NI")).getAdapterList().size() > i; i++) {
+			PcapIf pcapIf = ((NILayer) m_layer_mgr.getLayer("NI")).getAdapterObject(i);
+			nic_combo_box.addItem(pcapIf.getName());
+		}
+
+		nic_combo_box.addActionListener(new ActionListener() { // Event Listener
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JComboBox jcombo = (JComboBox) e.getSource();
+				adapter_number = jcombo.getSelectedIndex();
+				System.out.println("Index: " + adapter_number);
+				try {
+					ethernet_src_address.setText("");
+					ethernet_src_address.append(getMacAddress(((NILayer) m_layer_mgr.getLayer("NI"))
+							.getAdapterObject(adapter_number).getHardwareAddress()));
+
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
 	    nic_title = new JLabel("NIC 선택");
 	    nic_title.setBounds(50, 110, 170, 20);
 	    address_panel.add(nic_title);
 
 	    // 방법 2
-	    String[] adapterna= new String[((NILayer) m_layer_mgr.getLayer("NI")).m_pAdapterList.size()];
-
-	    for(int i=0;i<((NILayer) m_layer_mgr.getLayer("NI")).m_pAdapterList.size();i++)
-	    	adapterna[i] = ((NILayer) m_layer_mgr.getLayer("NI")).m_pAdapterList.get(i).getDescription();
-
-	    str_combo= new JComboBox(adapterna);
-	    str_combo.setBounds(150, 110, 165, 20);
-	    str_combo.setVisible(true);
-	    address_panel.add(str_combo);
-	    str_combo.addActionListener(new ActionListener() {
-	    	public void actionPerformed(ActionEvent e) {
-	            JComboBox cb = (JComboBox) e.getSource(); 
-	            adapter_number = cb.getSelectedIndex();
-
-	            try {
-	            	byte[] mac = ((NILayer) m_layer_mgr.getLayer("NI")).m_pAdapterList.get(adapter_number).getHardwareAddress();
-	            	final StringBuilder buf = new StringBuilder();
-	            	for(byte b:mac) {
-	            		if(buf.length()!=0) buf.append(":");
-	            		if(b>=0 && b<16) buf.append('0');
-	            		buf.append(Integer.toHexString((b<0)? b+256:b).toUpperCase());
-	            	}
-	            	byte[] ipSrcAddress = ((((NILayer)m_layer_mgr.getLayer("NI")).m_pAdapterList.get(adapter_number).getAddresses()).get(0)).getAddr().getData();
-	            	final StringBuilder buf2 = new StringBuilder();
-	            	for(byte b:ipSrcAddress) {
-	            		if(buf2.length()!=0) buf2.append(".");
-	            		buf2.append(b&0xff);
-	            	}
-	            	ip_src_address.setText(buf2.toString());
-	            	ethernet_src_address.setText(buf.toString());
-	            } catch (IOException e1) {
-	            	e1.printStackTrace();
-	            }
-	    	}
-	    });
+//	    String[] adapterna= new String[((NILayer) m_layer_mgr.getLayer("NI")).m_adapter_list.size()];
+//
+//	    for(int i=0;i<((NILayer) m_layer_mgr.getLayer("NI")).m_adapter_list.size();i++)
+//	    	adapterna[i] = ((NILayer) m_layer_mgr.getLayer("NI")).m_adapter_list.get(i).getDescription();
+//
+//	    str_combo= new JComboBox(adapterna);
+//	    str_combo.setBounds(150, 110, 165, 20);
+//	    str_combo.setVisible(true);
+//	    address_panel.add(str_combo);
+//	    str_combo.addActionListener(new ActionListener() {
+//	    	public void actionPerformed(ActionEvent e) {
+//	            JComboBox cb = (JComboBox) e.getSource();
+//	            adapter_number = cb.getSelectedIndex();
+//
+//	            try {
+//	            	byte[] mac = ((NILayer) m_layer_mgr.getLayer("NI")).m_adapter_list.get(adapter_number).getHardwareAddress();
+//	            	final StringBuilder buf = new StringBuilder();
+//	            	for(byte b:mac) {
+//	            		if(buf.length()!=0) buf.append(":");
+//	            		if(b>=0 && b<16) buf.append('0');
+//	            		buf.append(Integer.toHexString((b<0)? b+256:b).toUpperCase());
+//	            	}
+//	            	byte[] ipSrcAddress = ((((NILayer)m_layer_mgr.getLayer("NI")).m_adapter_list.get(adapter_number).getAddresses()).get(0)).getAddr().getData();
+//	            	final StringBuilder buf2 = new StringBuilder();
+//	            	for(byte b:ipSrcAddress) {
+//	            		if(buf2.length()!=0) buf2.append(".");
+//	            		buf2.append(b&0xff);
+//	            	}
+//	            	ip_src_address.setText(buf2.toString());
+//	            	ethernet_src_address.setText(buf.toString());
+//	            } catch (IOException e1) {
+//	            	e1.printStackTrace();
+//	            }
+//	    	}
+//	    });
 	    
 	    // ip주소 입력
 	    JLabel myIpTitle = new JLabel("IP Source");
@@ -579,8 +580,8 @@ public class ApplicationLayer extends JFrame implements BaseLayer {
 		        			ethernet_src[i] = (byte) Integer.parseInt(values_ethernet_src[i],16);
 		                }
 		        		 
-		        		((EthernetLayer) m_layer_mgr.getLayer("Ethernet")).SetEnetSrcAddress(ethernet_src);
-		                ((ARPLayer) m_layer_mgr.getLayer("ARP")).SetMacAddrSrcAddr(ethernet_src);
+		        		((EthernetLayer) m_layer_mgr.getLayer("Ethernet")).setEnetSrcAddress(ethernet_src);
+		                ((ARPLayer) m_layer_mgr.getLayer("ARP")).setHostMacAddr(ethernet_src);
 		                 
 		                String[] values_ip_source = ip_src_address.getText().split("\\.");
 		                byte[] ip_src = new byte[4];
@@ -588,9 +589,9 @@ public class ApplicationLayer extends JFrame implements BaseLayer {
 		                    ip_src[i] = (byte) Integer.parseInt(values_ip_source[i]);
 		                }
 		                 
-		                ((IPLayer) m_layer_mgr.getLayer("IP")).SetIPSrcAddress(ip_src);
-		                ((ARPLayer) m_layer_mgr.getLayer("ARP")).SetIPAddrSrcAddr(ip_src);
-		                ((NILayer) m_layer_mgr.getLayer("NI")).SetAdapterNumber(adapter_number);
+		                ((IPLayer) m_layer_mgr.getLayer("IP")).setIPSrcAddress(ip_src);
+		                ((ARPLayer) m_layer_mgr.getLayer("ARP")).setHostIpAddr(ip_src);
+		                ((NILayer) m_layer_mgr.getLayer("NI")).setAdapterNumber(adapter_number);
 		                
 		                ip_src_address.setEnabled(false);
 		        		ethernet_src_address.setEnabled(false);
